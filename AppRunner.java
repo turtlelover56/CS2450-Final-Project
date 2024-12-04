18a64abb6ffcf45175e3968abb872c2feee357fa
turtlelover56 <anadavis@gmail.com>
2024-12-04 14:01:02 -0800

    Added Intermission Code

--- AppRunner.java
+++ AppRunner.java
@@ -14,7 +14,7 @@ public class AppRunner {
         boolean ableToRun = true;
         Scanner sc = null;
         try {
-            sc = new Scanner(new File("Game Data.csv"));
+            sc = new Scanner(new File("Game Data.txt"));
         } catch (FileNotFoundException e) {
             System.out.println("Game Data file not found :(");
             ableToRun = false;
@@ -22,21 +22,22 @@ public class AppRunner {
         if (ableToRun) {
             String section = null;
             java.util.List<Attack> attackDex = new ArrayList<>();
-            java.util.List<Item> itemDex = new ArrayList<>();
+            java.util.List<Usable> itemDex = new ArrayList<>();
             java.util.List<Entity> entityDex = new ArrayList<>();
+            java.util.List<Intermission> intermissionDex = new ArrayList<>();
             // Read game data.
             while (sc.hasNextLine()) {
-                String[] line = sc.nextLine().split(",");
+                String[] line = sc.nextLine().split("\t");
                 if (line.length == 1)
                     section = line[0];
                 else {
                     switch (section) {
                         // For some reason the first line of the csv reads as this???
-                        case "ï»¿Attack":
+                        case "Attack":
                             attackDex.add(new Attack(Integer.parseInt(line[0]), new Usable(line[1], Boolean.parseBoolean(line[2]), Boolean.parseBoolean(line[3]), new Effect(Integer.parseInt(line[4]), Integer.parseInt(line[5]), Integer.parseInt(line[6]), Boolean.parseBoolean(line[7])))));
                             break;
                         case "Item":
-                            itemDex.add(new Item(new Usable(line[1], Boolean.parseBoolean(line[2]), Boolean.parseBoolean(line[3]), new Effect(Integer.parseInt(line[4]), Integer.parseInt(line[5]), Integer.parseInt(line[6]), Boolean.parseBoolean(line[7]))), Integer.parseInt(line[0])));
+                            itemDex.add(new Usable(line[0], Boolean.parseBoolean(line[1]), Boolean.parseBoolean(line[2]), new Effect(Integer.parseInt(line[3]), Integer.parseInt(line[4]), Integer.parseInt(line[5]), Boolean.parseBoolean(line[6]))));
                             break;
                         case "Entity":
                             int element;
@@ -85,6 +86,20 @@ public class AppRunner {
                             entity.setAttackPose(new ImageIcon(line[line.length-1]));
                             entityDex.add(entity);
                             break;
+                        case "Intermission":
+                            Usable itemOne = null;
+                            Usable itemTwo = null;
+                            int index = 0;
+                            while ((itemOne == null || itemTwo == null) && index < itemDex.size()) {
+                                Usable itemFound = itemDex.get(index);
+                                if (line[3].equals(itemFound.getName()))
+                                    itemOne = itemFound;
+                                else if (line[4].equals(itemFound.getName()))
+                                    itemTwo = itemFound;
+                                index++;
+                            }
+                            intermissionDex.add(new Intermission(line[0], line[1], line[2], new Item(itemOne, Integer.parseInt(line[5])), new Item(itemTwo, Integer.parseInt(line[6])), line[7], line[8]));
+                            break;
                         default:
                             System.out.println("Error: Section " + section + " not found.");
                     }
@@ -93,7 +108,8 @@ public class AppRunner {
             // Debugging prints.
             System.out.println(attackDex);
             System.out.println(itemDex);
-            System.out.println(entityDex);            
+            System.out.println(entityDex);  
+            System.out.println(intermissionDex);            
 
             
             // Create the frame.
@@ -115,9 +131,9 @@ public class AppRunner {
                     break; // Bad Practice :(
                 }
             }
-            for (Item item : itemDex) {
+            for (Usable item : itemDex) {
                 if (item.getName().equals("Health Potion") || item.getName().equals("Fire Bottle"))
-                    player.addItem(item);
+                    player.addItem(new Item(item, 3));
             }
             Enemy enemy = null;
             while (enemy == null) {
@@ -127,18 +143,23 @@ public class AppRunner {
                     enemy = new Enemy(chosenEntity);
             }
             Encounter encounter = new Encounter(enemy, player);
-            JPanel battlePane = PaneBuilder.buildBattlePanel(jfrm, encounter);
+            JPanel battlePane = PaneBuilder.buildBattlePanel(jfrm, cards, encounter);
             encounter.setBattlePane(battlePane);
+            // Build the intermission screen.
+            Intermission intermission = intermissionDex.get((int) (Math.random() * intermissionDex.size()));
+            JPanel intermissionPane = PaneBuilder.buildIntermissionPanel(jfrm, cards, intermission, player);
+
             // Build the death screen.
             JPanel deathPane = PaneBuilder.buildDeathPanel(jfrm, cards);
             
             // Add the panes to the frame.
             jfrm.add(startPane, "Start");
             jfrm.add(battlePane, "Battle");
+            jfrm.add(intermissionPane, "Intermission");
             jfrm.add(deathPane, "Death");
 
             // Debugging tool to change screens instantly.
-            //changeScreen(cards, jfrm.getContentPane(), "Death");
+            changeScreen(cards, jfrm.getContentPane(), "Intermission");
 
             // Make the frame visible.
             jfrm.setVisible(true);
