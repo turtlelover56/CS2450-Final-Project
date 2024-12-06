ef8bbf87edf8c6a5ffce7ce338a7716d37bac329
ToastyTristan <tristanduong1@gmail.com>
2024-12-06 06:46:38 -0800

    Reformatted all java files
    Changed spaces to tabs, made AppRunner.java more cleaner

--- Effect.java
+++ Effect.java
@@ -1,80 +1,82 @@
 public class Effect {
-    private int generalType; 
-    /*  0 - Damage
-        1 - Boost
-        2 - Heal */
-    private int specificType;
-    /*  -2 - Shield
-	    -1 - None
-	     0 - Fire
-	     1 - Water
-	     2 - Electricity
-	     3 - Wood */
-    private int power;
-    private int duration;
-    private boolean percent;
+	private int generalType; 
+	/* 0 - Damage
+	* 1 - Boost
+	* 2 - Heal 
+	*/
+	private int specificType;
+	/* -2 - Shield
+	* -1 - None
+	* 0 - Fire
+	* 1 - Water
+	* 2 - Electricity
+	* 3 - Wood 
+	*/
+	private int power;
+	private int duration;
+	private boolean percent;
 
-    // Constants
-    public static final int DAMAGE = 0;
-    public static final int BOOST = 1;
-    public static final int HEAL = 2;
+	// Constants
+	public static final int DAMAGE = 0;
+	public static final int BOOST = 1;
+	public static final int HEAL = 2;
 
-    public static final int SHIELD = -2;
-    public static final int NONE = -1;
-    public static final int FIRE = 0;
-    public static final int WATER = 1;
-    public static final int ELECTRICITY = 2;
-    public static final int WOOD = 3;
-    
-    // Constructors
-    /* Constructor with duration set to 0 for an instantaneous effect. */
-    public Effect(int generalType, int specificType, int power, boolean percent) {
-        this(generalType, specificType, power, 0, percent);
-    }
-    public Effect(int generalType, int specificType, int power, int duration, boolean percent) {
-        this.duration = duration;
-        this.generalType = generalType;
-        this.percent = percent;
-        this.power = power;
-        this.specificType = specificType;
-    }
+	public static final int SHIELD = -2;
+	public static final int NONE = -1;
+	public static final int FIRE = 0;
+	public static final int WATER = 1;
+	public static final int ELECTRICITY = 2;
+	public static final int WOOD = 3;
+	
+	// Constructors
+	/* Constructor with duration set to 0 for an instantaneous effect. */
+	public Effect(int generalType, int specificType, int power, boolean percent) {
+		this(generalType, specificType, power, 0, percent);
+	}
+	public Effect(int generalType, int specificType, int power, int duration, boolean percent) {
+		this.duration = duration;
+		this.generalType = generalType;
+		this.percent = percent;
+		this.power = power;
+		this.specificType = specificType;
+	}
 
-    // Getters and Setters
-    public int getGeneralType() {
-        return generalType;
-    }
-    public void setGeneralType(int generalType) {
-        this.generalType = generalType;
-    }
-    public int getSpecificType() {
-        return specificType;
-    }
-    public void setSpecificType(int specificType) {
-        this.specificType = specificType;
-    }
-    public int getPower() {
-        return power;
-    }
-    public void setPower(int power) {
-        this.power = power;
-    }
-    public int getDuration() {
-        return duration;
-    }
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-    public boolean isPercent() {
-        return percent;
-    }
-    public void setPercent(boolean percent) {
-        this.percent = percent;
-    }
-    
-    // toString
-    @Override
-    public String toString() {
-        return "Effect [generalType=" + generalType + ", specificType=" + specificType + ", power=" + power
-                + ", duration=" + duration + ", percent=" + percent + "]";
-    }
+	// Getters and Setters
+	public int getGeneralType() {
+		return generalType;
+	}
+	public void setGeneralType(int generalType) {
+		this.generalType = generalType;
+	}
+	public int getSpecificType() {
+		return specificType;
+	}
+	public void setSpecificType(int specificType) {
+		this.specificType = specificType;
+	}
+	public int getPower() {
+		return power;
+	}
+	public void setPower(int power) {
+		this.power = power;
+	}
+	public int getDuration() {
+		return duration;
+	}
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	public boolean isPercent() {
+		return percent;
+	}
+	public void setPercent(boolean percent) {
+		this.percent = percent;
+	}
+	
+	// toString
+	@Override
+	public String toString() {
+		return "Effect [generalType=" + generalType + ", specificType=" + specificType + ", power=" + power
+				+ ", duration=" + duration + ", percent=" + percent + "]";
+	}
 }
--- Encounter.java
+++ Encounter.java
@@ -1,135 +1,135 @@
 import javax.swing.JPanel;
 
 public class Encounter {
-    private Player player;
-    private Enemy enemy;
-    private JPanel battlePane;
+	private Player player;
+	private Enemy enemy;
+	private JPanel battlePane;
 
-    // Constructor
-    public Encounter(Enemy enemy, Player player) {
-        this.enemy = enemy;
-        this.player = player;
-        battlePane = null;
-    }
+	// Constructor
+	public Encounter(Enemy enemy, Player player) {
+		this.enemy = enemy;
+		this.player = player;
+		battlePane = null;
+	}
 
-    // public boolean runEncounter() {
-    //     if (roll(0, 1) == 1);
-    //         enemyTurn();
-    //     boolean running = checkStatus();
-    //     while (running) {
-    //         if (!playerTurn())
-    //             break; // Bad Practice :(
-    //         if (battlePane != null)
-    //             PaneBuilder.enableComponents(battlePane, false);
-    //         if (!enemyTurn())
-    //             break;
-    //         if (battlePane != null)
-    //             PaneBuilder.enableComponents(battlePane, true);
-    //     }
-    //     if (player.getCurrentHealth() <= 0)
-    //         return false;
-    //     else
-    //         return true;
-    // }
+	// public boolean runEncounter() {
+	//	 if (roll(0, 1) == 1);
+	//		 enemyTurn();
+	//	 boolean running = checkStatus();
+	//	 while (running) {
+	//		 if (!playerTurn())
+	//			 break; // Bad Practice :(
+	//		 if (battlePane != null)
+	//			 PaneBuilder.enableComponents(battlePane, false);
+	//		 if (!enemyTurn())
+	//			 break;
+	//		 if (battlePane != null)
+	//			 PaneBuilder.enableComponents(battlePane, true);
+	//	 }
+	//	 if (player.getCurrentHealth() <= 0)
+	//		 return false;
+	//	 else
+	//		 return true;
+	// }
 
-    protected int resolveAttack(Attack attack, boolean targetEnemy) {
-        if (Math.random() < attack.getAccuracy())
-            return resolveEffect(attack.getEffect(), targetEnemy);
-        else
-            return -1;
-    }
+	protected int resolveAttack(Attack attack, boolean targetEnemy) {
+		if (Math.random() < attack.getAccuracy())
+			return resolveEffect(attack.getEffect(), targetEnemy);
+		else
+			return -1;
+	}
 
-    protected int resolveItem(Item item, boolean targetEnemy) {
-        if (!item.decrement())
-            player.getInventory().remove(item);
-        return resolveEffect(item.getEffect(), targetEnemy);
-    }
+	protected int resolveItem(Item item, boolean targetEnemy) {
+		if (!item.decrement())
+			player.getInventory().remove(item);
+		return resolveEffect(item.getEffect(), targetEnemy);
+	}
 
-    /** Resolves the effect on the given target.
-     * 
-     * @param effect The effect to be resolved.
-     * @param targetEnemy True if the target is the enemy, false if the target is the player.
-     * @return 0 if the attack hits but is not very effective, 1 if the attack hits normally, and 2 if the attack is very effective.
-     */
-    protected int resolveEffect(Effect effect, boolean targetEnemy) {
-        int result = 1;
-        // Determine the target of the effect.
-        InstanceEntity target = targetEnemy ? enemy : player;
-        int power = effect.getPower();
-        switch (effect.getGeneralType()) {
-            // Damage
-            case 0:
-                int specificType = effect.getSpecificType();
-                if (specificType < -2 || specificType > 3)
-                    System.out.println("Error: Specific Type " + effect.getSpecificType() + " not recognized for General Type " + effect.getGeneralType() + ".");
-                else if (specificType != -1) {
-                    // Apply power bonus if effective against target type.
-                    int offset = specificType - 1;
-                    if (offset < 0)
-                        offset = 3;
-                    if (offset == target.getElement()) {
-                        power = (int) (power * 1.25);
-                        result = 2;
-                    }
-                    // Apply power debuff if not effective against target type.
-                    offset = specificType + 1;
-                    if (offset > 3)
-                        offset = 0;
-                    if (offset == target.getElement() || specificType == target.getElement()) {
-                        power = (int) (power * .75);
-                        result = 0;
-                    }
-                }
-                if (!effect.isPercent())
-                    target.changeCurrentHealth(-1 * power);
-                else
-                    target.changeCurrentHealth(-1 * (int) (power / 100.0 * target.getMaxHealth()));
-                break;
-            // Boost
-            case 1:
-                // Not implemented!
-                break;
-            // Heal
-            case 2:
-                // Specific effect not implemented.
-                if (!effect.isPercent())
-                    target.changeCurrentHealth(power);
-                else
-                    target.changeCurrentHealth((int) (power / 100.0 * target.getMaxHealth()));    
-                if (target.getCurrentHealth() > target.getMaxHealth())
-                    target.setCurrentHealth(target.getMaxHealth());
-                break;
-            // Unexpected type
-            default:
-                System.out.println("Error: General Type " + effect.getGeneralType() + " not recognized.");
-        }
-        return result;
-    }
+	/** Resolves the effect on the given target.
+	 * 
+	 * @param effect The effect to be resolved.
+	 * @param targetEnemy True if the target is the enemy, false if the target is the player.
+	 * @return 0 if the attack hits but is not very effective, 1 if the attack hits normally, and 2 if the attack is very effective.
+	 */
+	protected int resolveEffect(Effect effect, boolean targetEnemy) {
+		int result = 1;
+		// Determine the target of the effect.
+		InstanceEntity target = targetEnemy ? enemy : player;
+		int power = effect.getPower();
+		switch (effect.getGeneralType()) {
+			// Damage
+			case 0:
+				int specificType = effect.getSpecificType();
+				if (specificType < -2 || specificType > 3)
+					System.out.println("Error: Specific Type " + effect.getSpecificType() + " not recognized for General Type " + effect.getGeneralType() + ".");
+				else if (specificType != -1) {
+					// Apply power bonus if effective against target type.
+					int offset = specificType - 1;
+					if (offset < 0)
+						offset = 3;
+					if (offset == target.getElement()) {
+						power = (int) (power * 1.25);
+						result = 2;
+					}
+					// Apply power debuff if not effective against target type.
+					offset = specificType + 1;
+					if (offset > 3)
+						offset = 0;
+					if (offset == target.getElement() || specificType == target.getElement()) {
+						power = (int) (power * .75);
+						result = 0;
+					}
+				}
+				if (!effect.isPercent())
+					target.changeCurrentHealth(-1 * power);
+				else
+					target.changeCurrentHealth(-1 * (int) (power / 100.0 * target.getMaxHealth()));
+				break;
+			// Boost
+			case 1:
+				// Not implemented!
+				break;
+			// Heal
+			case 2:
+				// Specific effect not implemented.
+				if (!effect.isPercent())
+					target.changeCurrentHealth(power);
+				else
+					target.changeCurrentHealth((int) (power / 100.0 * target.getMaxHealth()));	
+				if (target.getCurrentHealth() > target.getMaxHealth())
+					target.setCurrentHealth(target.getMaxHealth());
+				break;
+			// Unexpected type
+			default:
+				System.out.println("Error: General Type " + effect.getGeneralType() + " not recognized.");
+		}
+		return result;
+	}
 
-    /* Checks the status of the encounter. 
-     * Returns false if player or enemy's health is below or equal to 0.
-     * Else returns true. */
-    protected boolean checkStatus() {
-        return !(player.getCurrentHealth() <= 0 || enemy.getCurrentHealth() <= 0);
-    }
+	/* Checks the status of the encounter. 
+	 * Returns false if player or enemy's health is below or equal to 0.
+	 * Else returns true. */
+	protected boolean checkStatus() {
+		return !(player.getCurrentHealth() <= 0 || enemy.getCurrentHealth() <= 0);
+	}
 
-    // Getters/Setters
-    public Player getPlayer() {
-        return player;
-    }
-    public void setPlayer(Player player) {
-        this.player = player;
-    }
-    public Enemy getEnemy() {
-        return enemy;
-    }
-    public void setEnemy(Enemy enemy) {
-        this.enemy = enemy;
-    }
-    public JPanel getBattlePane() {
-        return battlePane;
-    }
-    public void setBattlePane(JPanel battlePane) {
-        this.battlePane = battlePane;
-    }
+	// Getters/Setters
+	public Player getPlayer() {
+		return player;
+	}
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+	public Enemy getEnemy() {
+		return enemy;
+	}
+	public void setEnemy(Enemy enemy) {
+		this.enemy = enemy;
+	}
+	public JPanel getBattlePane() {
+		return battlePane;
+	}
+	public void setBattlePane(JPanel battlePane) {
+		this.battlePane = battlePane;
+	}
 }
